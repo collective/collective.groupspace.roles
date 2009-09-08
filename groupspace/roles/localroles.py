78e021630ffdf520ffe6a29938fee342adfeab6f
Maik Röder <maikroeder@gmail.com>
2009-09-08 20:14:47 +0000

    =local roles
    git-svn-id: http://svn.plone.org/svn/collective/groupspace.roles/trunk@97065 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ groupspace/roles/localroles.py
@@ -0,0 +1,32 @@
+from borg.localrole.interfaces import ILocalRoleProvider
+from Products.GrufSpaces.interface import IGroupSpace
+from zope.interface import implements
+from zope.component import adapts
+
+class LocalRoles(object):
+    """Provide a local role manager for group spaces
+    """
+    implements(ILocalRoleProvider)
+    adapts(IGroupSpace)
+
+    def __init__(self, context):
+        self.context = context
+
+    def getAllRoles(self):
+        for user_id, user_roles in self.context.user_roles.items():
+            for role in user_roles:
+                yield (user_id, role)
+        for group_id, group_roles in self.context.group_roles.items():
+            for role in group_roles:
+                yield (group_id, role)
+
+    def getRoles(self, principal_id):
+        roles = set()
+        if principal_id in self.context.user_roles.keys():
+            for role in self.context.user_roles[principal_id]:
+                roles.add(role)
+        if principal_id in self.context.group_roles.keys():
+            for role in self.context.group_roles[principal_id]:
+                roles.add(role)
+        return roles    
+
