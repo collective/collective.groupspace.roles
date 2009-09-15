2bab0440a6d666d6d5dc06f05722dbae7c417ac8
Maik Röder <maikroeder@gmail.com>
2009-09-15 14:28:58 +0000

    =More careful when accessing the attributes
    git-svn-id: http://svn.plone.org/svn/collective/groupspace.roles/trunk@97503 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- groupspace/roles/localroles.py
+++ groupspace/roles/localroles.py
@@ -13,20 +13,24 @@ class LocalRoles(object):
         self.context = context
 
     def getAllRoles(self):
-        for user_id, user_roles in self.context.user_roles.items():
-            for role in user_roles:
-                yield (user_id, role)
-        for group_id, group_roles in self.context.group_roles.items():
-            for role in group_roles:
-                yield (group_id, role)
+        if not self.context.user_roles is None:
+            for user_id, user_roles in self.context.user_roles.items():
+                for role in user_roles:
+                    yield (user_id, role)
+        if not self.context.group_roles is None:
+            for group_id, group_roles in self.context.group_roles.items():
+                for role in group_roles:
+                    yield (group_id, role)
 
     def getRoles(self, principal_id):
         roles = set()
-        if principal_id in self.context.user_roles.keys():
-            for role in self.context.user_roles[principal_id]:
-                roles.add(role)
-        if principal_id in self.context.group_roles.keys():
-            for role in self.context.group_roles[principal_id]:
-                roles.add(role)
+        if not self.context.user_roles is None:
+            if principal_id in self.context.user_roles.keys():
+                for role in self.context.user_roles[principal_id]:
+                    roles.add(role)
+        if not self.context.group_roles is None:
+            if principal_id in self.context.group_roles.keys():
+                for role in self.context.group_roles[principal_id]:
+                    roles.add(role)
         return roles    
 
