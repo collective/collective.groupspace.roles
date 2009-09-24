1a17ce425789c0bd8a526fbca6f0509bf0d715c9
Maik Röder <maikroeder@gmail.com>
2009-09-24 14:48:30 +0000

    =Adding Portlet that shows the available GroupSpaces
    git-svn-id: http://svn.plone.org/svn/collective/groupspace.roles/trunk@98171 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ groupspace/roles/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+rolesMessageFactory = MessageFactory('groupspace.roles')
+
+def initialize(context):
+    """ Intializer called when used as a Zope 2 product.
+    """
