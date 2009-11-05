4eb5a4008e1f61be8bba966d3e523c34713adee2
Maik Röder <maikroeder@gmail.com>
2009-11-05 13:34:54 +0000

    =centralize all roles specific interfaces here
    git-svn-id: http://svn.plone.org/svn/collective/collective.groupspace.roles/trunk@101359 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/groupspace/roles/interfaces.py
+++ collective/groupspace/roles/interfaces.py
@@ -1,5 +1,6 @@
 from zope.interface import Interface
 from zope.interface import Attribute
+from zope import schema
 from zope.component.interfaces import IObjectEvent
 
 class ILocalGroupSpacePASRoles(Interface):
@@ -21,3 +22,22 @@ class ILocalGroupSpacePASRolesChangeEvent(IObjectEvent):
     new_user_roles = Attribute("The new user roles for the object.")
     old_group_roles = Attribute("The old group roles for the object.")
     new_group_roles = Attribute("The new group roles for the object.")
+
+class IRolesPageRole(Interface):
+    """
+    A named utility providing information about roles that are managed by the
+    roles page.
+    
+    Utility names should correspond to the role name.
+    
+    A user will be able to delegate the given role if a utility can be found
+    and the user has the required_permission (or it's None).
+    """
+
+    title = schema.TextLine(title=u"A friendly name for the role")
+
+    required_permission = schema.TextLine(
+        title=u"Permission required to manage this local role",
+        required=False
+    )
+
